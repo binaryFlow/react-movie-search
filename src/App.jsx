481abe47254e9bbc3cf7b35d150713cf92d7bd23
Anas Akkaoui <an.akkaoui@gmail.com>
2025-09-05 21:55:10 +0200

    feat: all movie cards displayed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -75,7 +76,7 @@ function App() {
             ) : (
               <ul>
                 {movies.map((movie) => (
-                  <p key={movie.id} className="text-white">{movie.title}</p>
+                  <MovieCard movie={movie} />
                 ))}
               </ul>
             )
